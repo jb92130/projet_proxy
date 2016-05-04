cb6bf319da07eb3cef10f2f447fd89d71c7a02d3
HenryTelecom <henry.ung@telecomnancy.net>
2016-05-04 19:25:46 +0200

    maj

--- Projet_Proxy/proxy.c
+++ Projet_Proxy/proxy.c
@@ -17,6 +17,14 @@
 #include <netdb.h>
 #include <pthread.h>
 
+#define BUFSIZE 66000
+#define SOMAXCONN 20
+
+struct params_thread {
+    int* sockFd;
+    pthread_t threadId;
+    int* counter;
+};
 
 void printLog(char* str) {
     printf("::Log:: %s", str);
@@ -26,10 +34,168 @@ void printError(char* str) {
     printf("::Error:: %s", str);
 }
 
-void operation(void* param) {
-    int sockCliFd = (int) param;
-    printLog("Creating thread for the client\n");
+char* get_host(char* httpRequest){
+    unsigned short i = 0, j = 0;
+    char* buffer = strstr(httpRequest, "Host: " );
+    char* host = (char *)malloc(256 * sizeof(char));
+    while( buffer != NULL && buffer[i] != '\n')
+        i++;
+    for(j = 6; j < i-1; j++)
+        host[j - 6] = buffer[j];
+    host[j-6+1] = '\0';
+    return host;
+}
+
+int openTCP (char* addr) {
     
+    /**
+     * Déclaration des variables
+     */
+    
+    int sockfd, isHttps = 0;
+    struct sockaddr_in  serv_addr;
+    struct hostent* serv_host;
+    
+    //printf("ADDR:%s %d\n", addr, strlen(addr));
+    
+    /*
+     * Ouvrir une socket (a TCP socket)
+     */
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) <0) {
+        perror("servmulti : Probleme socket\n");
+        exit (2);
+    }
+    
+    char https[strlen(addr)];
+    int find=0;
+    
+    if((strstr(addr, ":443")) != NULL) {
+        isHttps=1;
+        strcpy(https, addr);
+        for (int i=0; i<strlen(addr); ++i) {
+            if (addr[i]==':') {
+                find=1;
+            }
+            if (find) {
+                https[i]='\0';
+            }
+        }
+        addr = https;
+    }
+    
+    /*char* https = strstr(addr, ":443");
+     if (https != NULL) {
+     //addr = strcmp(addr, ":443");
+     printf("LEN: %d", strlen(addr));
+     addr[strlen(addr)-1]='\0';
+     }*/
+    
+    //printf("ADDR:%s\n", addr);
+    //printf("HTTPS:%s", https);
+    
+    /*
+     * On récupère l'host
+     */
+    serv_host = gethostbyname(addr);
+    if (serv_host == NULL) {
+        //perror("Host introuvable...\n");
+        return -1;
+    }
+    
+    /*
+     * Lier le host à la socket TCP
+     */
+    memset( (char*) &serv_addr,0, sizeof(serv_addr) );
+    serv_addr.sin_family = serv_host->h_addrtype;
+    serv_addr.sin_addr = *(struct in_addr*) serv_host->h_addr_list[0];
+    
+    if (isHttps) {
+        serv_addr.sin_port = htons((ushort) atoi("443"));
+    }
+    else {
+        serv_addr.sin_port = htons((ushort) atoi("80"));
+    }
+    
+    //printf("IP Address: %s\n", inet_ntoa(serv_addr.sin_addr));
+    
+    
+    if (connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+        //perror("Server : Error connection to destination\n");
+        return -1;
+    }
+    
+    printf("Server : Connection established...\n");
+    
+    return sockfd;
+}
+
+void operation(void* params) {
+    printLog("Start operation\n");
+    
+    /**
+     * Variable declaration
+     */
+    struct params_thread* pt = (struct params_thread*) params;
+    int sockCliFd = * (pt->sockFd), sockServFd;
+    int nrcv, nsnd;
+    char msg[BUFSIZE];
+    
+    /**
+     * msg initialization & reading request on sockCliFd
+     */
+    memset((char*) msg, 0, sizeof(msg));
+    if ((nrcv = read(sockCliFd, msg, sizeof(msg)-1)) < 0) {
+        printError("Proxy : Error read\n");
+        exit(1);
+    }
+    msg[nrcv] = '\0';
+    
+    printf("%s", msg);
+    
+    char* dest_addr = get_host(msg);
+    int err = 0;
+    
+    /**
+     * Opening TCP connection
+     */
+    if ((sockServFd = openTCP(dest_addr)) < 0) {
+        printError("Server : Error opening TCP connection to destination\n");
+        err = 1;
+    }
+    
+    /**
+     * Writing to destination
+     */
+    if (!err && (nsnd = write(sockServFd, msg, nrcv)) < 0) {
+        printError("Server : Error writing to destination\n");
+        err = 1;
+    }
+    
+    /**
+     * Reading from destination
+     */
+    if (!err && (nrcv = read(sockServFd, msg, sizeof(msg)-1)) < 0) {
+        printError("Client : Error reading message from destination\n");
+        err = 1;
+    }
+    
+    /**
+     * Writing to client
+     */
+    if (!err && (nsnd = write(sockCliFd, msg, nrcv)) < 0) {
+        printError("Client : Error writing message to client\n");
+        err = 1;
+    }
+    
+    printf("%d", * (pt->sockFd));
+    
+    printLog("End operation\n");
+    
+    (*(pt->counter))--;
+    
+    close(sockServFd);
+    close(sockCliFd);
+    pthread_cancel(pt->threadId);
 }
 
 int main (int argc,char *argv[]) {
@@ -39,9 +205,10 @@ int main (int argc,char *argv[]) {
     /**
      * Variable delerations
      */
-    int sockfd, sockCliFd, sockServFd;
+    int sockfd;
     struct sockaddr_in  serv_addr, cli_addr;
     socklen_t clilen;
+    int counter = 0;
     
     /**
      * Check the number of arguments
@@ -92,7 +259,7 @@ int main (int argc,char *argv[]) {
          * Accepting client 
          */
         clilen = sizeof(cli_addr);
-        sockCliFd = accept(sockfd, (struct sockaddr*) &cli_addr, &clilen);
+        int sockCliFd = accept(sockfd, (struct sockaddr*) &cli_addr, &clilen);
         
         if (sockCliFd < 0) {
             printError("Server : Error accept\n");
@@ -105,12 +272,19 @@ int main (int argc,char *argv[]) {
          * Multi threading
          */
         pthread_t t;
+        struct params_thread pt;
+        pt.sockFd = &sockCliFd;
+        pt.threadId = t;
+        pt.counter = &counter;
         
-        if (pthread_create (&t, NULL, (void*) operation, sockCliFd) < 0) {
+        printLog("Creating thread for the client\n");
+        
+        if (pthread_create (&t, NULL, (void*) operation, &pt) < 0) {
             printError("Server : Error thread creation\n");
             exit (1);
         }
         
+        counter++;
     }
     
     
